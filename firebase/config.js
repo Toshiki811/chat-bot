2ded7d906c70f42c9a32d0dfdeecf82b15dd4960
Toshiki811 <65066211+Toshiki811@users.noreply.github.com>
2021-01-05 10:37:47 +0900

    chat

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAUDTxA6TCz1bRJASv3Ux_2XyTCVOtsHgc",
+  authDomain: "chatbot-demo-3efb0.firebaseapp.com",
+  projectId: "chatbot-demo-3efb0",
+  storageBucket: "chatbot-demo-3efb0.appspot.com",
+  messagingSenderId: "542706828123",
+  appId: "1:542706828123:web:490564f1666f2cf6eb107e",
+  measurementId: "G-Q2SDJ7239N"
+};
+
+export {firebaseConfig};
